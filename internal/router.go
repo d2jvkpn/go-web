43695fabf3c1b07c6012789713f5918755a21176
d2jvkpn <chenbin2018@protonmail.com>
2022-05-14 22:48:07 +0800

    ...

--- internal/router.go
+++ internal/router.go
@@ -13,7 +13,7 @@ func LoadAPI(engi *gin.Engine, handlers ...gin.HandlerFunc) {
 	open.GET("/hello", func(ctx *gin.Context) {
 		key := "Authorization"
 		fmt.Printf("~~~ Header %s: %s\n", key, ctx.GetHeader(key))
-		ctx.JSON(http.StatusOK, gin.H{"code": 0, "message": "ok"})
+		ctx.JSON(http.StatusOK, gin.H{"code": 0, "message": "ok", "data": gin.H{}})
 	})
 
 	open.POST("/login", func(ctx *gin.Context) {
