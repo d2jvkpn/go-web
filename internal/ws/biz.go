1960059a7a97bf06bed124cff039f08d4ef03311
d2jvkpn <chenbin2018@protonmail.com>
2022-05-19 13:56:38 +0800

    fixed Melody set handles concurrency problem

---
+++ internal/ws/biz.go
@@ -0,0 +1,125 @@
+package ws
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/olahol/melody.v1"
+)
+
+var (
+	_ClientId uint64
+	_MelHello *melody.Melody
+)
+
+func init() {
+	_MelHello = melody.New()
+	// log.Printf("%+v\n", _MelHello.Config)
+	_MelHello.Config.PingPeriod = 10 * time.Second
+	_MelHello.Upgrader.CheckOrigin = func(req *http.Request) bool { return true }
+
+	_MelHello.HandleConnect(func(sess *melody.Session) {
+		var (
+			exists bool
+			client *Client
+		)
+
+		if client, exists = sess.Keys["client"].(*Client); !exists {
+			return
+		}
+
+		log.Printf(">>> hello new ws connection: %q, addr=%s\n", client, client.Address)
+		data := map[string]interface{}{
+			"code":    0,
+			"type":    "client",
+			"message": fmt.Sprintf("%s, nice to meet you!", client.Name),
+			"data":    gin.H{"clientId": client.Id},
+		}
+
+		log.Printf("--> %q send client information\n", client)
+		bts, _ := json.Marshal(data)
+		_ = sess.Write(bts)
+	})
+
+	_MelHello.HandleDisconnect(func(sess *melody.Session) {
+		var (
+			exists bool
+			client *Client
+		)
+
+		if client, exists = sess.Keys["client"].(*Client); !exists {
+			return
+		}
+
+		log.Printf("<<< hello ws disconnected: %q\n", client)
+	})
+
+	_MelHello.HandleError(func(sess *melody.Session, err error) {
+		var (
+			exists bool
+			client *Client
+		)
+
+		if client, exists = sess.Keys["client"].(*Client); !exists {
+			return
+		}
+
+		log.Printf("!!! hello ws error: %q, error=%q\n", client, err)
+	})
+
+	_MelHello.HandlePong(func(sess *melody.Session) {
+		var (
+			exists bool
+			client *Client
+		)
+
+		if client, exists = sess.Keys["client"].(*Client); !exists {
+			return
+		}
+
+		client.PongTime = time.Now()
+		log.Printf("<~~ %q recv pong\n", client)
+	})
+
+	_MelHello.HandleMessage(func(sess *melody.Session, msg []byte) {
+		var (
+			exists bool
+			client *Client
+		)
+
+		if client, exists = sess.Keys["client"].(*Client); !exists {
+			return
+		}
+
+		log.Printf("<-- %q recv: %q\n", client, msg)
+
+		// handle(msg)
+		// _MelHello.Broadcast(msg)
+	})
+}
+
+type Client struct {
+	Id          string
+	Address     string
+	Name        string
+	ConnectTime time.Time
+	PongTime    time.Time
+}
+
+func NewClient(addr, name string) *Client {
+	return &Client{
+		Id:          fmt.Sprintf("%04d", atomic.AddUint64(&_ClientId, 1)),
+		Address:     addr,
+		Name:        name,
+		ConnectTime: time.Now(),
+	}
+}
+
+func (client Client) String() string {
+	return fmt.Sprintf("name=%s, id=%s", client.Name, client.Id)
+}
--- internal/ws/hello.go
+++ internal/ws/hello.go
@@ -1,103 +1,18 @@
 package ws
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
-	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"gopkg.in/olahol/melody.v1"
 )
-
-var (
-	_ClientId uint64
-	_MelHello *melody.Melody = melody.New()
-)
-
-type Client struct {
-	Id          string
-	Ip          string
-	Name        string
-	ConnectTime time.Time
-	PongTime    time.Time
-}
-
-func NewClient(id, ip, name string) *Client {
-	return &Client{
-		Id:          id,
-		Ip:          ip,
-		Name:        name,
-		ConnectTime: time.Now(),
-	}
-}
-
-func (client Client) String() string {
-	return fmt.Sprintf("name=%s, id=%s", client.Name, client.Id)
-}
 
 func hello(ctx *gin.Context) {
-	var (
-		once   *sync.Once
-		client *Client
-	)
-
-	once = new(sync.Once)
-
-	client = NewClient(
-		fmt.Sprintf("%04d", atomic.AddUint64(&_ClientId, 1)),
-		ctx.ClientIP(),
+	client := NewClient(
+		ctx.Request.RemoteAddr, // ctx.ClientIP(),
 		ctx.DefaultQuery("name", "World"),
 	)
 
-	// log.Printf("%+v\n", _MelHello.Config)
-	_MelHello.Config.PingPeriod = 10 * time.Second
-	_MelHello.Upgrader.CheckOrigin = func(req *http.Request) bool { return true }
-
-	_MelHello.HandleConnect(func(sess *melody.Session) {
-		log.Printf(">>> hello new ws connection: %q, ip=%s\n", client, client.Ip)
-	})
-
-	_MelHello.HandleDisconnect(func(sess *melody.Session) {
-		log.Printf("<<< hello ws disconnected: %q\n", client)
-	})
-
-	_MelHello.HandleError(func(sess *melody.Session, err error) {
-		log.Printf("!!! hello ws error: %q, error=%q\n", client, err)
-	})
-
-	_MelHello.HandlePong(func(sess *melody.Session) {
-		client.PongTime = time.Now()
-		log.Printf("<~~ %q recv pong\n", client)
-	})
-
-	_MelHello.HandleMessage(func(sess *melody.Session, msg []byte) {
-		log.Printf("<-- %q recv: %q\n", client, msg)
-
-		once.Do(func() {
-			var client *Client
-			if sess.Keys != nil {
-				client, _ = sess.Keys["client"].(*Client)
-			}
-			log.Printf("~~~ client: %+v\n", client)
-
-			data := map[string]interface{}{
-				"code":    0,
-				"type":    "client",
-				"message": fmt.Sprintf("%s, nice to meet you!", client.Name),
-				"data":    gin.H{"clientId": client.Id},
-			}
-
-			log.Printf("--> %q send client information\n", client)
-			bts, _ := json.Marshal(data)
-			_ = sess.Write(bts)
-		})
-
-		// m.Broadcast(msg)
-	})
+	log.Printf("================ %s\n", client)
 
 	// _ = _MelHello.HandleRequest(ctx.Writer, ctx.Request)
 	_ = _MelHello.HandleRequestWithKeys(
