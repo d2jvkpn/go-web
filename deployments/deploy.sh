8af254cfe227f19bdb8a1187c97fa5b47c750307
d2jvkpn <chenbin2018@protonmail.com>
2022-05-28 20:15:41 +0800

    fixed deploy problems

--- deployments/deploy.sh
+++ deployments/deploy.sh
@@ -11,7 +11,7 @@ PORT=$3
 export APP_ENV=${APP_ENV} gitBranch=${gitBranch} PORT=${PORT}
 envsubst < ${_path}/deploy.yaml > docker-compose.yaml
 
-docker-compose pull
+# docker-compose pull
 [[ ! -z "$(docker ps --all --quiet --filter name=goapp_$APP_ENV)" ]] &&
   docker rm -f goapp_$APP_ENV
 # docker-compose down for running containers only, not stopped containers
