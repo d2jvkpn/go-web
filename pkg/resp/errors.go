cb5cf9d840d35b4974faa226d6b371b440b01930
d2jvkpn <chenbin2018@protonmail.com>
2022-06-15 18:03:58 +0800

    ...

--- pkg/resp/errors.go
+++ pkg/resp/errors.go
@@ -11,24 +11,33 @@ import (
 // 1..=99   business error
 // 100...   unexpected error
 
-func ErrParseFailed(cause error, opts ...Option) (err *HttpError) {
-	opts = append(opts, Skip(2))
-	err = NewHttpError(cause, http.StatusBadRequest, -1, opts...)
-	if err.Msg == "" {
-		err.Msg = "parse data failed"
-	}
-	return
-}
-
 func ErrBadRequest(cause error, opts ...Option) (err *HttpError) {
 	opts = append(opts, Skip(2))
-	err = NewHttpError(cause, http.StatusBadRequest, -2, opts...)
+	err = NewHttpError(cause, http.StatusBadRequest, -1, opts...)
 	if err.Msg == "" {
 		err.Msg = "bad request"
 	}
 	return
 }
 
+func ErrParseFailed(cause error) (err *HttpError) {
+	err = NewHttpError(cause, http.StatusBadRequest, -2, Skip(2))
+	if err.Msg == "" {
+		err.Msg = "parse data failed"
+	}
+	return
+}
+
+func ErrInvalidParameter(err error, msg string) (out *HttpError) {
+	return NewHttpError(err, http.StatusBadRequest, -3, Skip(2))
+}
+
+////
+func ErrNotFound(err error) (out *HttpError) {
+	return NewHttpError(err, http.StatusNotFound, -4, Skip(2), Msg("not found"))
+}
+
+////
 func ErrServerError(cause error) (err *HttpError) {
 	return NewHttpError(
 		cause,
