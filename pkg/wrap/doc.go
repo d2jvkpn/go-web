0d79669d87cf170cf01700fefb79d2cb6dea7029
d2jvkpn <chenbin2018@protonmail.com>
2022-06-22 23:32:30 +0800

    ...

--- pkg/wrap/doc.go
+++ pkg/wrap/doc.go
@@ -1,10 +1,15 @@
 /*
 Third Party Packags:
 - github.com/gin-gonic/gin
+- github.com/spf13/viper
+
 - go.uber.org/multierr
 - go.uber.org/zap
 - go.uber.org/zap/zapcore
 - gopkg.in/natefinch/lumberjack.v2
-- github.com/spf13/viper
+
+- github.com/prometheus/client_golang/prometheus
+- github.com/prometheus/client_golang/prometheus/promauto
+- github.com/prometheus/client_golang/prometheus/promhttp
 */
 package wrap
