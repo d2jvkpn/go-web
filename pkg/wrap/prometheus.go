d54c2d361b822087e857953eabbd1e826a29eb16
d2jvkpn <chenbin2018@protonmail.com>
2022-06-23 13:21:49 +0800

    ...

--- pkg/wrap/prometheus.go
+++ pkg/wrap/prometheus.go
@@ -14,25 +14,18 @@ import (
 
 func NewPrometheusMonitor() gin.HandlerFunc {
 	totalRequests := prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "http_requests_total",
-			Help: "Number of get requests.",
-		},
+		prometheus.CounterOpts{Name: "http_requests_total", Help: "Number of get requests."},
 		[]string{"path"},
 	)
 
 	responseStatus := prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "response_status",
-			Help: "Status of HTTP response",
-		},
+		prometheus.CounterOpts{Name: "response_status", Help: "Status of HTTP response"},
 		[]string{"status"},
 	)
 
 	httpDuration := promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name: "http_response_time_seconds",
-			Help: "Duration of HTTP requests.",
+			Name: "http_response_time_seconds", Help: "Duration of HTTP requests.",
 		},
 		[]string{"path"},
 	)
