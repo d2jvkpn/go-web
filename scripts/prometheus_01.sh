f5e69701c030a3e8af97f3589e0fa4e2569e344e
d2jvkpn <chenbin2018@protonmail.com>
2022-06-23 14:42:26 +0800

    ...

--- scripts/prometheus_01.sh
+++ scripts/prometheus_01.sh
@@ -1,3 +1,34 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+# https://gabrieltanner.org/blog/collecting-prometheus-metrics-in-golang/
+# https://hub.docker.com/r/prom/prometheus
+# https://hub.docker.com/r/grafana/grafana/tags
+
+#### create prometheus job configs
+mkdir -p configs
+
+cat > configs/prometheus.yaml << EOF
+global:
+  scrape_interval:     15s
+  evaluation_interval: 15s
+
+scrape_configs:
+- job_name: prometheus
+  static_configs:
+  - targets: ['prometheus:9090']
+
+- job_name: go-web_dev
+  metrics_path: /prometheus
+  static_configs:
+  - targets:
+    - go-web_dev:8080
+EOF
+
+#### run services
+cat > docker-compose.yaml << EOF
 version: "3"
 
 services:
@@ -49,3 +80,19 @@ networks:
     name: go-web_dev
     driver: bridge
     external: false
+EOF
+
+docker-compose up -d
+
+docker exec -it go-web_dev curl prometheus:9090/metrics
+docker exec -it go-web_dev curl go-web_dev:8080/prometheus
+
+#### reset grafana password
+docker exec -it grafana grafana-cli admin reset-admin-password PASSWORD
+
+# grafana url: http:localhost:3000, admin PASSWORD
+# add prometheus data source: http://prometheus:9090
+
+curl http://localhost:8080/api/v1/open/hello
+
+ls -alh /var/lib/docker/volumes/go-web_grafana-storage/_data
