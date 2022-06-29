2198a3d9a0767a6eefe139a5c9d324dabc872c76
d2jvkpn <chenbin2018@protonmail.com>
2022-06-29 23:19:11 +0800

    vender support

---
+++ deployments/build_dev.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env bash
+set -eu -o pipefail
+_wd=$(pwd)
+_path=$(dirname $0 | xargs -i readlink -f {})
+
+
+function onExit {
+    git checkout dev # --force
+    rm -f build.lock
+}
+mkfifo build.lock
+trap onExit EXIT
+
+gitBranch=dev
+image="registry.cn-shanghai.aliyuncs.com/d2jvkpn/go-web"
+tag=$gitBranch
+
+####
+b1="$(git rev-parse --abbrev-ref HEAD)" # current branch
+uncommitted=$(git status --short)
+unpushed=$(git diff origin/$b1..HEAD --name-status)
+
+git checkout --force $gitBranch
+buildTime=$(date +'%FT%T%:z')
+gitCommit=$(git rev-parse --verify HEAD) # git log --pretty=format:'%h' -n 1
+gitTime=$(git log -1 --format="%at" | xargs -I{} date -d @{} +%FT%T%:z)
+# git tag $git_tag
+# git push origin $git_tag
+gitTreeState="clean"
+
+uncommitted=$(git status --short)
+unpushed=$(git diff origin/$gitBranch..HEAD --name-status)
+[[ ! -z "$uncommitted$unpushed" ]] && gitTreeState="dirty"
+
+# prev=$(docker images $image:$tag -q)
+echo ">>> build image: $image:$tag..."
+
+docker build --no-cache --file ${_path}/Dockerfile.dev --tag $image:$tag \
+  --build-arg=buildTime="$buildTime"       \
+  --build-arg=gitBranch="$gitBranch"       \
+  --build-arg=gitCommit="$gitCommit"       \
+  --build-arg=gitTime="$gitTime"           \
+  --build-arg=gitTreeState="$gitTreeState" \
+  ./
+
+docker image prune --force --filter label=stage=go-web_builder &> /dev/null
+
+#### push image
+echo ">>> push image: $image:$tag..."
+docker tag $image:$tag $image:${tag}-xx
+docker push $image:$tag
+docker push $image:${tag}-xx
+
+
+images=$(docker images --filter "dangling=true" --quiet $image)
+for img in $images; do docker rmi $img || true; done &> /dev/null
+# [[ ! -z "$prev" ]] && docker rmi "$prev" &> /dev/null || true
